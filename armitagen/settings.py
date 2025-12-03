3c9a01f7799ea04a5d0483f932d3f4bfede7e08a
mmd-punisher <mohammadmoosapoor4@gmail.com>
2025-12-03 17:02:24 +0330

    fix: new templates - council app

--- armitagen/settings.py
+++ armitagen/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     # Apps
     'home.apps.HomeConfig',
     'academy.apps.AcademyConfig',
